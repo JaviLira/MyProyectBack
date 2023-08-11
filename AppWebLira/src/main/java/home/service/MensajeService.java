f4cb89c08d0b950e1ddd86914f991f4fd057a4be
Francisco Javier Lira Sánchez <101715862+JaviLira@users.noreply.github.com>
2023-08-11 10:41:38 +0200

    Servicios basicos creados

--- AppWebLira/src/main/java/home/service/MensajeService.java
+++ AppWebLira/src/main/java/home/service/MensajeService.java
@@ -1,5 +1,27 @@
 package home.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import home.model.Mensajes;
+import home.repository.MensajeRepo;
+
+@Service("Mensaje")
 public class MensajeService {
 
+	@Autowired
+	private MensajeRepo repoMensaje;
+	
+	public Mensajes buscarMensaje(Long id) {
+		return repoMensaje.findById(id).orElse(null);
+	}
+	
+	public List<Mensajes> findAll() {
+		return repoMensaje.findAll();
+	}
+	
+	
+	
 }
--- AppWebLira/src/main/java/home/service/UserBService.java
+++ AppWebLira/src/main/java/home/service/UserBService.java
@@ -1,5 +1,25 @@
 package home.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import home.model.UserBase;
+import home.repository.UserRepo;
+
+@Service("UserB")
 public class UserBService {
 
+	@Autowired
+	private UserRepo repoUser;
+	
+	public UserBase buscarUsuario(String id) {
+		return repoUser.findById(id).orElse(null);
+	}
+	
+	public List<UserBase> findAll() {
+		return repoUser.findAll();
+	}
+	
 }
